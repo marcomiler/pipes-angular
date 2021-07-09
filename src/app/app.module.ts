98a728079b6379c2a95a00d16cccc62118f86146
marcomiler <miler.rmarco@gmail.com>
2021-07-09 00:58:26 -0500

    rutas-navegacion-primeNg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    AppRouterModule,
+    BrowserModule,
+    SharedModule,
+    VentasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
